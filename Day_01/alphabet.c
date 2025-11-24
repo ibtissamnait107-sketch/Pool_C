ab0c45179536b78cd567ebb58fc5c656194ebe83
ibtissamnait107-sketch <ibtissamnait107@gmail.com>
2025-11-24 12:44:40 +0100

    hello

---
+++ Day_01/alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+
+char c;
+for(c='a';c<='z';c++){
+	printf("%c\t",c);
+}
+return 0;
+}
+
---
+++ Day_01/character.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+
+char a;
+printf("enter a");
+scanf("%c",&a);
+printf("%c",a);
+return 0;
+}
+
